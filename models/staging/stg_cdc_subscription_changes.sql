926e3ba5386229a98c240099e8493428231d9636
apexnomad10@gmail.com <apexnomad10@gmail.com>
2026-06-27 21:29:52 +0000

    add staging model for support tickets and started working on cdc changes

---
+++ models/staging/stg_cdc_subscription_changes.sql
@@ -0,0 +1,20 @@
+with source as (
+        select * {{ source("telavox_raw", "cdc_subscription_changes") }}
+),
+
+renamed as (
+    select 
+        cast(change_id as string) as change_id,
+        cast(subscription_id as string) as subscription_id,
+        cast(company_id as string) as company_id,
+        cast(operaton as string) as operation,
+        cast(changed_at as timestamp) as changed_at,
+        cast(plan_type as string) as plan_type,
+        cast(monthly_price_sek as int) as monthly_price_sek,
+        cast(status as string) as status,
+        cast(_cdc_sequence as int) as cdc_sequence
+    
+    from source
+)
+
+select * from renamed
